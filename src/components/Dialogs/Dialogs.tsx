93cf3729ecd15ca5dc9684ac7fe3e65d18c73203
EHOTbl <stalker_mogilev@mail.ru>
2023-03-17 16:29:11 +0300

    22lesson

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,8 +1,36 @@
 import s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
-const Dialogs =()=>{
+const Dialogs = () => {
     return (
-        <div>"Hello</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'> Dima</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Lesha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                     <NavLink to='/dialogs/3'>Igor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                     <NavLink to='/dialogs/4'>Sveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                     <NavLink to='/dialogs/5'>Ignat</NavLink>
+                </div>
+                <div className={s.dialog}>
+                     <NavLink to='/dialogs/6'>Oleg</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Kamasutra</div>
+                <div className={s.message}>Tester</div>
+            </div>
+        </div>
     )
 }
 
